74ba54c711fb57412e597c05a8049245647edc96
dwingmz <gmzdwn6@gmail.com>
2026-05-02 17:29:38 -0500

    style: actualiza imagen de perfil en la página principal

--- src/profile.config.js
+++ src/profile.config.js
@@ -1,12 +1,12 @@
 const profile = {
   name: "Darwin Steven Gomez",
-  role: "Ingeniero y Desarrollador de software Full Stack",
-  location: "Popayan/Cauca, Colombia",
+  role: "Ingeniero y Desarrollador de software",
+  location: "Cali/Valle del Cauca, Colombia",
   bio: "Me enfoco en construir productos multiplataforma, web, moviles entre otros con tecnologias como Node.js, React, Python Django y usar buenas prácticas. Me gustan los retos y compartir lo que aprendo.",
-  avatarUrl: "https://res.cloudinary.com/dbtzbgfdw/image/upload/v1760425636/yo_ncmudv.jpg",
+  avatarUrl: "https://res.cloudinary.com/dmj1zbrmp/image/upload/v1777756390/darwin_ykkes8.jpg",
   email: "speakdwin@gmail.com",
-  website: "",
-  github: "https://github.com/Dstevengmz",
+  website: "https://blogdarwin.vercel.app/",
+  github: "https://github.com/Dstevengmz", 
   linkedin: "https://www.linkedin.com/in/dstevengmz",
   skills: ["JavaScript", "Python", "Dart", "Java", "Vite", "NodeJS","Django","Flask","Mysql","PostgreSQL","MongoDB","Docker", "Git", "GitHub", "Grafana","Dart"],
 };
