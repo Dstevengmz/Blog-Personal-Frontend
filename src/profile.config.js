33dc35216d59b93b3c2af1ed56c0848ffbb38ba3
dwingmz <gmzdwn6@gmail.com>
2026-05-02 17:35:23 -0500

    style: actualizacion imagen de perfil en la página principal

--- src/profile.config.js
+++ src/profile.config.js
@@ -3,7 +3,7 @@ const profile = {
   role: "Ingeniero y Desarrollador de software",
   location: "Cali/Valle del Cauca, Colombia",
   bio: "Me enfoco en construir productos multiplataforma, web, moviles entre otros con tecnologias como Node.js, React, Python Django y usar buenas prácticas. Me gustan los retos y compartir lo que aprendo.",
-  avatarUrl: "https://res.cloudinary.com/dmj1zbrmp/image/upload/v1777756390/darwin_ykkes8.jpg",
+  avatarUrl: "https://res.cloudinary.com/dmj1zbrmp/image/upload/v1777761220/darwinrecortado_vcg4jl.png",
   email: "speakdwin@gmail.com",
   website: "https://blogdarwin.vercel.app/",
   github: "https://github.com/Dstevengmz", 
